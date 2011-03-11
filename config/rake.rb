0a3ad83f808fc344fb125ee3baefc1ce9c78484e
Aaron Unger <aunger@concord.org>
2011-03-11 14:44:41 -0500

    Customize the rake config.

--- config/rake.rb
+++ config/rake.rb
@@ -6,22 +6,22 @@
 ###
 
 # The company name - used for SSL certificates, and in srvious other places
-COMPANY_NAME = "Example Com"
+COMPANY_NAME = "Concord Consortium"
 
 # The Country Name to use for SSL Certificates
 SSL_COUNTRY_NAME = "US"
 
 # The State Name to use for SSL Certificates
-SSL_STATE_NAME = "Several"
+SSL_STATE_NAME = "MA"
 
 # The Locality Name for SSL - typically, the city
-SSL_LOCALITY_NAME = "Locality"
+SSL_LOCALITY_NAME = "Concord"
 
 # What department?
-SSL_ORGANIZATIONAL_UNIT_NAME = "Operations"
+SSL_ORGANIZATIONAL_UNIT_NAME = "Development"
 
 # The SSL contact email address
-SSL_EMAIL_ADDRESS = "ops@example.com"
+SSL_EMAIL_ADDRESS = "webmaster@concord.org"
 
 # License for new Cookbooks
 # Can be :apachev2 or :none
