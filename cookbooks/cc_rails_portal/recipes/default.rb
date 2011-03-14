fe99c3d3411faa57100da22a2024243b167b0167
Aaron Unger <aunger@concord.org>
2011-03-14 16:18:11 -0400

    Make sure the portal files are owned by a specified user. Use the chef git resource to check out files.

--- cookbooks/cc_rails_portal/recipes/default.rb
+++ cookbooks/cc_rails_portal/recipes/default.rb
@@ -20,35 +20,43 @@ web_app "project" do
   notifies :reload, resources(:service => "apache2"), :delayed
 end
 
+user node[:cc_rails_portal][:user] do
+  comment "portal user"
+  system true
+  shell "/bin/false"
+end
+
 if node[:cc_rails_portal][:checkout] == "true"
-  execute "clone-portal-app" do
-    cwd "/tmp"
-    command "git clone #{node[:cc_rails_portal][:source_url]} portal"
-  end
-
-  execute "rename-portal-app" do
-    cwd "/tmp"
-    command "mv portal #{node[:cc_rails_portal][:root]}"
-  end
-
-  execute "bundle-install" do
-    cwd node[:cc_rails_portal][:root]
-    command "git checkout #{node[:cc_rails_portal][:source_branch]}"
+  git node[:cc_rails_portal][:root] do
+    repository node[:cc_rails_portal][:source_url]
+    reference node[:cc_rails_portal][:source_branch]
+    action :sync
   end
 end
 
 execute "bundle-install" do
+  user node[:cc_rails_portal][:user]
   cwd node[:cc_rails_portal][:root]
   command "bundle install"
 end
 
 execute "setup-xportal-app" do
+  user node[:cc_rails_portal][:user]
   cwd node[:cc_rails_portal][:root]
   command "ruby config/setup.rb -n '#{node[:cc_rails_portal][:name]}' -D #{node[:cc_rails_portal][:theme]} -u root -p '' -t #{node[:cc_rails_portal][:theme]} -y -q -f"
 end
 
 execute "initialize-xportal-database" do
+  user node[:cc_rails_portal][:user]
   cwd node[:cc_rails_portal][:root]
-  environment ({'RAILS_ENV' => 'production'})
+  environment ({'RAILS_ENV' => node[:rails][:environment]})
   command "rake db:migrate:reset"
+end
+
+if node[:cc_rails_portal][:checkout] == "true"
+  execute "portal-setup" do
+    user node[:cc_rails_portal][:user]
+    cwd node[:cc_rails_portal][:root]
+    command "rake app:setup:new_app"
+  end
 end
