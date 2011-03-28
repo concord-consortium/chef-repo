762f0db82f3c79b252eeb6cf26f713a16a31f868
Aaron Unger <aunger@concord.org>
2011-03-28 16:09:57 -0400

    Only try to disable the default site if it is current enabled.

--- cookbooks/apache2/recipes/disable_default_site.rb
+++ cookbooks/apache2/recipes/disable_default_site.rb
@@ -1,4 +1,5 @@
 execute "disable-default-site" do
   command "a2dissite default"
   notifies :reload, resources(:service => "apache2"), :delayed
+  only_if {File.exists?(File.join(node[:apache][:dir], "sites-enabled", "000-default")) || File.exists?(File.join(node[:apache][:dir], "sites-enabled", "default"))}
 end
