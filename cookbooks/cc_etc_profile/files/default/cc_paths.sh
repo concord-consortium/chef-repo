27cb904f7fa225aed463dd1b42705a1662a3ffc9
Noah Paessel <knowuh@gmail.com>
2011-10-17 16:23:42 -0400

    add iptables, move paths to cc_paths.sh

---
+++ cookbooks/cc_etc_profile/files/default/cc_paths.sh
@@ -0,0 +1,5 @@
+# cc_paths.sh
+# On centos /etc/profile.d/*.sh gets sourced
+# So we add our path modifications here
+PATH=$PATH:/usr/local/bin:$HOME/bin
+export PATH
--- cookbooks/cc_etc_profile/recipes/default.rb
+++ cookbooks/cc_etc_profile/recipes/default.rb
@@ -2,14 +2,18 @@
 # Cookbook Name:: cc_etc_profile
 # Recipe:: default
 #
-# Copyright 2011, YOUR_COMPANY_NAME
+# Copyright 2011, Concord Consortium
 #
 # All rights reserved - Do Not Redistribute
 #
 #
 
-cookbook_file "/etc/profile" do
-  source "profile"
+cookbook_file "/etc/profile.d/cc_paths.sh" do
+  source "cc_paths.sh"
   mode "0644"
 end
 
+cookbook_file "/etc/sysconfig/iptables" do
+  source "iptables"
+  mode "0600"
+end
