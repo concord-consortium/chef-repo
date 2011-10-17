a9418f3682028c655060808e83dbc3c0e4931f25
Noah Paessel <knowuh@gmail.com>
2011-10-17 16:35:43 -0400

    don't provision unless [:cc_rails_app][:setup]

--- cookbooks/cc_rails_app/recipes/portal.rb
+++ cookbooks/cc_rails_app/recipes/portal.rb
@@ -22,52 +22,58 @@ root = File.join(root,"current") if config[:capistrano_folders]
 
 
 
-if node[:cc_rails_app][:setup]
-
-  # run the config setup script
-  # TODO: this probably needs to be reworked
-  # so that it functions with more recent versions
-  execute "setup-portal-settings" do
-    user node[:cc_rails_app][:user]
-    cwd root
-    command "ruby config/setup.rb -n '#{config[:name]}' -D #{config[:theme]} -u #{config[:mysql][:username]} -p '#{config[:mysql][:password]}' -t #{config[:theme]} -y -q -f"
-    not_if do
-      File.exists?(File.join(config[:root], "skip-provisioning"))
-    end
+unless node[:cc_rails_app][:setup]
+  # This file indicates that we don't wish to do anything
+  # potentially destructive reset dabatabases, 
+  # reset app settings &etc.  
+  file File.join(config[:root], "skip-provisioning") do
+    action :touch
   end
-
-  # intialize the database
-  cc_rails_update_database "portal" do
-    app :portal
-  end
-  
-  # run rake setup task
-  execute "portal-setup" do
-    user node[:cc_rails_app][:user]
-    cwd root
-    environment ({'RAILS_ENV' => node[:rails][:environment]})
-    command "yes | rake app:setup:new_app"
-    not_if do
-      File.exists?(File.join(config[:root], "skip-provisioning"))
-    end
-  end
-
-  # FIXME This should get done, but for some reason, tends to make chef hang...
-  # execute "compile-sass" do
-  #   user node[:cc_rails_app][:user]
-  #   cwd root
-  #   environment ({'RAILS_ENV' => node[:rails][:environment]})
-  #   command "compass --sass-dir public/stylesheets/sass/ --css-dir public/stylesheets/ -s compressed --force"
-  # end
-
-  # execute "package-assets" do
-  #   user node[:cc_rails_app][:user]
-  #   cwd root
-  #   environment ({'RAILS_ENV' => node[:rails][:environment]})
-  #   command "rake asset:packager:build_all"
-  # end
 end
 
+# run the config setup script
+# TODO: this probably needs to be reworked
+# so that it functions with more recent versions
+execute "setup-portal-settings" do
+  user node[:cc_rails_app][:user]
+  cwd root
+  command "bundle exec ruby config/setup.rb -n '#{config[:name]}' -D #{config[:theme]} -u #{config[:mysql][:username]} -p '#{config[:mysql][:password]}' -t #{config[:theme]} -y -q -f"
+  not_if do
+    File.exists?(File.join(config[:root], "skip-provisioning"))
+  end
+end
+
+# intialize the database
+cc_rails_update_database "portal" do
+  app :portal
+end
+
+# run rake setup task
+execute "portal-setup" do
+  user node[:cc_rails_app][:user]
+  cwd root
+  environment ({'RAILS_ENV' => node[:rails][:environment]})
+  command "yes | bundle exec rake app:setup:new_app"
+  not_if do
+    File.exists?(File.join(config[:root], "skip-provisioning"))
+  end
+end
+
+# FIXME This should get done, but for some reason, tends to make chef hang...
+# execute "compile-sass" do
+#   user node[:cc_rails_app][:user]
+#   cwd root
+#   environment ({'RAILS_ENV' => node[:rails][:environment]})
+#   command "compass --sass-dir public/stylesheets/sass/ --css-dir public/stylesheets/ -s compressed --force"
+# end
+
+# execute "package-assets" do
+#   user node[:cc_rails_app][:user]
+#   cwd root
+#   environment ({'RAILS_ENV' => node[:rails][:environment]})
+#   command "rake asset:packager:build_all"
+# end
+
 file File.join(config[:root], "skip-provisioning") do
-  action :touch
+  action :create
 end
